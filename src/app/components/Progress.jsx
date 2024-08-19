9eba42f306fe37116658c3a6073726083e3264ad
Jasmine P. Vo <99847030+jasminepvo@users.noreply.github.com>
2024-08-19 16:40:22 -0500

    clean up ui

--- src/app/components/Progress.jsx
+++ src/app/components/Progress.jsx
@@ -18,7 +18,7 @@ export default function Progress() {
 
     return (
         <div className="flex flex-col items-center">
-            <h2 className="text-lg font-subheading mb-4">{randomQuote}</h2>
+            <h2 className="text-xl font-semibold font-subheading mb-4">{randomQuote}</h2>
             <div className="grid grid-cols-7 gap-2">
                 {days.map((day) => (
                     <div key={day} className="w-8 h-8 border border-gray-400 rounded-md">
